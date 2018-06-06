7df338ab4ce13a221ba9c1623a07f0a017c31e39
Matvienko Oleg <OMatvienko@ics.perm.ru>
2018-06-06 14:22:16 +0500

    Initial commit

---
+++ ConcurrencyDemo/ConcurrencyDemo/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConcurrencyDemo
+{
+    class Program
+    {
+        static string[] _products = new string[] { "Lays", "Cheetos", "Pringles", "Estrella", "Русская картошка" };
+
+        static Dictionary<string, int> _stockBalances = new Dictionary<string, int>();
+
+        static void Main(string[] args)
+        {
+            foreach (var product in _products)
+            {
+                _stockBalances[product] = 0;
+            }
+
+            var worktime = TimeSpan.FromSeconds(2);
+            Task t1 = Task.Run(() => DoWork(worktime));
+            Task t2 = Task.Run(() => DoWork(worktime));
+            Task t3 = Task.Run(() => DoWork(worktime));
+            Task t4 = Task.Run(() => DoWork(worktime));
+
+            Task.WaitAll(t1, t2, t3, t4);
+
+            PrintLeftovers();
+            Console.ReadKey();
+        }
+
+
+        static void BuyProduct(string productName, int amount)
+        {
+            _stockBalances[productName] += amount;
+        }
+
+        static bool TrySellProduct(string productName)
+        {
+            if (_stockBalances[productName] > 0)
+            {
+                _stockBalances[productName]--;
+                return true;
+            }
+            return false;
+        }
+
+        static void DoWork(TimeSpan workTime)
+        {
+            var rand = new Random(Thread.CurrentThread.ManagedThreadId);
+            var start = DateTime.Now;
+            while (DateTime.Now - start < workTime)
+            {
+                Thread.Sleep(rand.Next(100));
+                bool buy = (rand.Next(6) == 0);
+                string product = _products[rand.Next(_products.Length)];
+                if (buy)
+                {
+                    int amount = rand.Next(9) + 1;
+                    BuyProduct(product, amount);
+                    DisplayPurchase(product, amount);
+                }
+                else
+                {
+                    var success = TrySellProduct(product);
+                    DisplaySaleAttempt(success, product);
+                }
+            }
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} finished.");
+        }
+
+        private static void DisplaySaleAttempt(bool success, string product)
+        {
+            if (success) Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} sold {product}");
+            else Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} couldn't sell {product}");
+        }
+
+        private static void DisplayPurchase(string product, int amount)
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} purchased {amount} items of {product}");
+        }
+
+        private static void PrintLeftovers()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var productBalance in _stockBalances)
+            {
+                Console.WriteLine($"{productBalance.Key}: {productBalance.Value}");
+            }
+        }
+    }
+}
---
+++ ConcurrencyDemo/WpfDeadlockDemo/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDeadlockDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        async Task DoSmthAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(2));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var task = DoSmthAsync();
+
+            task.Wait();
+        }       
+
+        async Task DoSmthRightAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(2)).ConfigureAwait(false);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var task = DoSmthRightAsync();
+
+            task.Wait();
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            await DoSmthRightAsync();
+        }
+    }
+}
